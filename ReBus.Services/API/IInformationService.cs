5bd2826dd5b02e8ef6b3025a7c3c961512f2f91c
Razvan Jurca <razvi09@gmail.com>
2012-05-26 20:57:24 +0300

    Updates

---
+++ ReBus.Services/API/IInformationService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using ReBus.Model;
+
+namespace ReBus.Services.API
+{
+    public interface IInformationService
+    {
+        decimal GetTicketPrice();
+
+        IDictionary<int, decimal> GetSubscriptionPrices();
+
+        IEnumerable<Line> GetLines();
+
+        Bus GetBus(Guid bus);
+
+        Ticket GetTicket(Guid ticket);
+
+        Subscription GetSubscription(Subscription subscription);
+    }
+}
---
+++ ReBus.Services/InformationService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ReBus.Model;
+using ReBus.Services.API;
+
+namespace ReBus.Services
+{
+    public class InformationService : IInformationService
+    {
+        public decimal GetTicketPrice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDictionary<int, decimal> GetSubscriptionPrices()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Line> GetLines()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Bus GetBus(Guid bus)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Ticket GetTicket(Guid ticket)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Subscription GetSubscription(Subscription subscription)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
