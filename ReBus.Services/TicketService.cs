36a146ff345dfae756a8dbb9ef2e32180410f4e0
Razvan Jurca <razvi09@gmail.com>
2012-05-26 16:28:24 +0300

    Added method declarations to the ticket service interface

---
+++ ReBus.Services/TicketService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using ReBus.Model;
+using ReBus.Services.API;
+
+namespace ReBus.Services
+{
+    public class TicketService : ITicketService
+    {
+        public Ticket BuyTicket(Account account, Bus bus)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Ticket GetActiveTicket(Account account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Ticket> GetTicketHistory(Account account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Ticket> GetTicketHistory(Account account, int limit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Ticket> GetTicketHistory(Account account, DateTime before, int limit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Ticket> GetTicketHistory(Account account, DateTime after)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
